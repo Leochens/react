a765835b3422808b3622a48330aa765f52f279e0
Leochens·ZHL <18332518328@163.com>
2018-08-08 16:16:56 +0800

    up

--- hw_7_30/src/App.js
+++ hw_7_30/src/App.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import './App.css';
+import OPSider from './containers/Sider/Sider';
+
+import { Layout, Menu, Icon, Breadcrumb } from 'antd';
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
 
 class App extends Component {
   render() {
     return (
-      <div >
-        {this.props.children}
-      </div >
+      <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">小年糕TIA</Menu.Item>
+            <Menu.Item key="2">摄影课</Menu.Item>
+            <Menu.Item key="3">绘画课</Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <OPSider />
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>首页</Breadcrumb.Item>
+              <Breadcrumb.Item>摄影训练营</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+              {this.props.children}
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
     );
   }
 }
