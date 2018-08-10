87687581714e5d55dad67dcdab7c2dc6f71434af
Leochens·ZHL <18332518328@163.com>
2018-08-10 13:20:25 +0800

    up

--- hw_7_30/src/Test/test.js
+++ hw_7_30/src/Test/test.js
@@ -4,11 +4,11 @@ import TestActionSheet from './WeUI/TestActionSheet';
 import TestProgress from './WeUI/TestProgress';
 import TestSlider from './WeUI/TestSlider';
 import TestSwitch from './WeUI/TestSwitch';
-
+// import './test.less';
 export default class Test extends Component {
     render() {
         return (
-            <div>
+            <div className="xxxxxxx">
                 <h3>渲染ActionSheet请单独打开</h3>
                 <TestDialog />
                 <TestActionSheet />
