1dc4c40bccb0ceffef02a8bc724eb080efb38e3c
Leochens·ZHL <18332518328@163.com>
2018-08-16 12:47:15 +0800

    基本逻辑优化实现

--- gama2048/src/tools/index.js
+++ gama2048/src/tools/index.js
@@ -40,100 +40,33 @@ export const isGameOver = ({ col, row }) => {
 }
 const copyMap = (oldMap) => oldMap.slice();
 
-const swap = (map, { curRow, curCol }, { preRow, preCol }) => {
-    const tmp = map[curRow][curCol];
-    map[curRow][curCol] = map[preRow][preCol];
-    map[preRow][preCol] = tmp;
-}
-const stackByDirections = (map, direction, wantMerge = 0) => {
-
-    const len = 4;
-    let initRowId = 0;
-    let initColId = 0;
-    let rowIdWillPlus = true;
-    let colIdWillPlus = true;
-    let prePos = {};
-    const getPrePos = (preRow = -1, preCol = -1) => {
-        prePos.preRow = preRow;
-        prePos.preCol = preCol
-    }
-    switch (direction) {
-        case 'up':
-            initColId = len - 1;
-            colIdWillPlus = false;
-            initRowId = len - 1;
-            rowIdWillPlus = false;
-            break;
-        case 'left':
-            initColId = len - 1;
-            colIdWillPlus = false;
-            break
-        case 'down':
-
-
-            break;
-        case 'right':
-            initRowId = len - 1;
-            rowIdWillPlus = false;
-            break
-        default: break;
-    }
-    for (let rowId = initRowId; rowIdWillPlus ? rowId < len - 1 : rowId >= 0; rowIdWillPlus ? rowId++ : rowId--) {
-        for (let colId = initColId; colIdWillPlus ? colId < len : colId >= 0; colIdWillPlus ? colId++ : colId--) {
-            switch (direction) {
-                case 'left':
-                    getPrePos(rowId, colId - 1)
-                    break;
-                case 'right':
-                    getPrePos(rowId, colId + 1)
-                    break;
-                case 'up':
-                    getPrePos(rowId - 1, colId)
-                    break;
-                case 'down':
-                    getPrePos(rowId + 1, colId)
-                    break;
-                default: break;
-            }
-            const { preRow, preCol } = prePos;
-            const pre = map[preRow][preCol];
-            const cur = map[rowId][colId];
-            if (cur != 0 && pre === 0) {
-                map[preRow][preCol] = cur
-                map[rowId][colId] = 0;
-            } else if (wantMerge && cur !== 0 && cur === pre) {     //保存下要改变的位置
-                map[preRow][preCol] = pre * 2;
-                map[rowId][colId] = 0;
-            } else continue
-        }
-    }
-}
 
 const moveLeft = (oldMap) => {
     const newMap = copyMap(oldMap);
     let flag = 0;
     for (let r = 0; r < 4; r++) {
-        let i, nextI, len, m;
-        len = 4;
+        let i, nextNoZeroPos, len, m;
+        const arr = newMap[r];
+        len = arr.length;
         for (i = 0; i < len; i += 1) {
-            //先找nextI
-            nextI = -1;
+            //先找nextNoZeroPos
+            nextNoZeroPos = -1;
             for (m = i + 1; m < len; m++) {
-                if (newMap[r][m] !== 0) {
-                    nextI = m;
+                if (arr[m] !== 0) {
+                    nextNoZeroPos = m;
                     break;
                 }
             }
-            if (nextI !== -1) {
+            if (nextNoZeroPos !== -1) {
                 //存在下个不为0的位置
-                if (newMap[r][i] === 0) {
-                    newMap[r][i] = newMap[r][nextI];
-                    newMap[r][nextI] = 0;
+                if (arr[i] === 0) {
+                    arr[i] = arr[nextNoZeroPos];
+                    arr[nextNoZeroPos] = 0;
                     i -= 1;
                     flag = 1;
-                } else if (newMap[r][i] === newMap[r][nextI]) {
-                    newMap[r][i] = newMap[r][i] * 2;
-                    newMap[r][nextI] = 0;
+                } else if (arr[i] === arr[nextNoZeroPos]) {
+                    increaseNum =  arr[i] = arr[i] * 2;
+                    arr[nextNoZeroPos] = 0;
                     flag = 1;
                 }
             }
@@ -167,27 +100,27 @@ const moveUp = (oldMap) => {
     let increaseNum = 0;
     let flag = 0;
     for (let r = 0; r < 4; r++) {
-        let i, nextI, len, m;
+        let i, pos, len, m;
         len = 4;
         for (i = 0; i < len; i += 1) {
-            //先找nextI
-            nextI = -1;
+            //先找pos
+            pos = -1;
             for (m = i + 1; m < len; m++) {
                 if (newMap[m][r] !== 0) {
-                    nextI = m;
+                    pos = m;
                     break;
                 }
             }
-            if (nextI !== -1) {
+            if (pos !== -1) {
                 //存在下个不为0的位置
                 if (newMap[i][r] === 0) {
-                    newMap[i][r] = newMap[nextI][r];
-                    newMap[nextI][r] = 0;
+                    newMap[i][r] = newMap[pos][r];
+                    newMap[pos][r] = 0;
                     i -= 1;
                     flag = 1;
-                } else if (newMap[i][r] === newMap[nextI][r]) {
-                    newMap[i][r] = newMap[i][r] * 2;
-                    newMap[nextI][r] = 0;
+                } else if (newMap[i][r] === newMap[pos][r]) {
+                    increaseNum = newMap[i][r] = newMap[i][r] * 2;
+                    newMap[pos][r] = 0;
                     flag = 1;
                 }
             }
@@ -217,27 +150,28 @@ const moveRight = (oldMap) => {
     let increaseNum = 0;
     let flag = 0;
     for (let r = 0; r < 4; r++) {
-        let i, nextI, len, m;
-        len = 4;
-        for (i = len - 1; i >= 0; i--) {
-            //先找nextI
-            nextI = -1;
-            for (m = i - 1; m >= 0; m--) {
-                if (newMap[r][m] !== 0) {
-                    nextI = m;
+        let i, nextNoZeroPos, len, m;
+        const arr = newMap[r];
+        len = arr.length;
+        for (i = len -1 ; i > 0 ; i --) {
+            //先找nextNoZeroPos
+            nextNoZeroPos = -1;
+            for (m = i - 1 ; m >= 0 ; m--) {
+                if (arr[m] !== 0) {
+                    nextNoZeroPos = m;
                     break;
                 }
             }
-            if (nextI !== -1) {
+            if (nextNoZeroPos !== -1) {
                 //存在下个不为0的位置
-                if (newMap[r][i] === 0) {
-                    newMap[r][i] = newMap[r][nextI];
-                    newMap[r][nextI] = 0;
-                    i -= 1;
+                if (arr[i] === 0) {
+                    arr[i] = arr[nextNoZeroPos];
+                    arr[nextNoZeroPos] = 0;
+                    i += 1;
                     flag = 1;
-                } else if (newMap[r][i] === newMap[r][nextI]) {
-                    newMap[r][i] = newMap[r][i] * 2;
-                    newMap[r][nextI] = 0;
+                } else if (arr[i] === arr[nextNoZeroPos]) {
+                    increaseNum =  arr[i] = arr[i] * 2;
+                    arr[nextNoZeroPos] = 0;
                     flag = 1;
                 }
             }
@@ -267,27 +201,27 @@ const moveDowm = (oldMap) => {
     let increaseNum = 0;
     let flag = 0;
     const len = 4;
-    for (let r = len-1; r >= 0 ; r --) {
-        let i, nextI, m;
+    for (let r = len - 1; r >= 0; r--) {
+        let i, pos, m;
         for (i = len - 1; i >= 0; i--) {
-            //先找nextI
-            nextI = -1;
+            //先找pos
+            pos = -1;
             for (m = i - 1; m >= 0; m--) {
                 if (newMap[m][r] !== 0) {
-                    nextI = m;
+                    pos = m;
                     break;
                 }
             }
-            if (nextI !== -1) {
+            if (pos !== -1) {
                 //存在下个不为0的位置
                 if (newMap[i][r] === 0) {
-                    newMap[i][r] = newMap[nextI][r];
-                    newMap[nextI][r] = 0;
-                    i -= 1;
+                    newMap[i][r] = newMap[pos][r];
+                    newMap[pos][r] = 0;
+                    i += 1;
                     flag = 1;
-                } else if (newMap[i][r] === newMap[nextI][r]) {
-                    newMap[i][r] = newMap[i][r] * 2;
-                    newMap[nextI][r] = 0;
+                } else if (newMap[i][r] === newMap[pos][r]) {
+                    increaseNum = newMap[i][r] = newMap[i][r] * 2;
+                    newMap[pos][r] = 0;
                     flag = 1;
                 }
             }
