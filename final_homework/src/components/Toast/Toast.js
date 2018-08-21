95d7739c0a2a7d6d795be32bc0ddb48f3b9b869e
Leochens·ZHL <18332518328@163.com>
2018-08-21 22:38:50 +0800

    up

---
+++ final_homework/src/components/Toast/Toast.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Toast.less';
+export default class Toast extends Component {
+  state = {};
+  
+  render() {
+    return (
+      <div>
+        Toast
+      </div>
+    );
+  }
+}
